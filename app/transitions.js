309d258d2dc5cfcf920fed341fae8238afe9ef8d
Erik Bryn <erik.bryn@gmail.com>
2015-06-03 23:01:47 -0700

    Use higher quality thumbnail, leave old video preview in place

--- app/transitions.js
+++ app/transitions.js
@@ -4,7 +4,7 @@ export default function(){
   this.transition(
     this.hasClass('video-player'),
     this.use('explode', {
-      pickNew: 'img',
+      pick: 'img',
       use: ['to-and-from-sidebar', { duration }]
     }, {
       use: ['fade', { duration: duration/2 }]
