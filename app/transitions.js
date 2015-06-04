15855c4b4144a70df5bd1c91b188957a407111cc
Chris Thoburn <runspired@gmail.com>
2015-06-03 23:43:26 -0500

    use explode-from-parent

--- app/transitions.js
+++ app/transitions.js
@@ -2,9 +2,10 @@ export default function(){
   this.transition(
     // this.fromRoute('videos'),
     // this.toRoute('video'),
-    this.childOf('body'),
-    this.use('explode', {
+    this.use('explode-from-parent', {
       matchBy: 'data-video-id',
+      parentSelector: '.layout',
+      childSelector: '.sidebar [data-video-id]',
       use: ['fly-to', { duration: 300, easing: 'spring'}]
     }),
     this.debug()
